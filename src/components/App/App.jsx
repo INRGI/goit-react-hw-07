c6c769039985cde5fefc46ca465ba813381049b8
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-07 14:05:17 +0100

    api operations

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,20 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../ContactForm";
 import ContactList from "../ContactList";
 import Filter from "../Filter";
 import { ContactTitle, Container, EmptyTitle, PhoneTitle } from "./App.styled";
 import { getContacts } from "../../redux/selectors";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/operations";
 
 
 const App = () => {
   const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
       <Container>
