c17b7ce329a51ab65b35cfcb71a0db892e34c823
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-07 14:20:24 +0100

    Update selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,14 @@
-export const getContacts = state => state.contacts;
 
-export const getFilters = state => state.filters.inputValue;
+export const selectContacts = (state) => state.contacts.items;
+
+export const selectIsLoading = (state) => state.contacts.isLoading;
+
+export const selectError = (state) => state.contacts.error;
+
+export const selectFilter = (state) => state.filters.inputValue;
+
+export const selectVisibleContacts = (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+};
